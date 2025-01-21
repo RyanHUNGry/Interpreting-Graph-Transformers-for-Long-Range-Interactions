66f4d734c131d0c1c3faa2f77d9180fbc5cdc19c
Ryan Hung <ryhung@ucsd.edu>
2025-01-20 17:16:49 -0800

    structicism

--- src/models/attention/analyze.py
+++ src/models/attention/analyze.py
@@ -1,1 +1,11 @@
-def 
+import matplotlib.pyplot as plt
+from torch import bincount
+from collections import Counter
+class AttentionAnalyzer:
+    def __init__(self, attention_mat):
+        self.attention_mat = attention_mat
+
+    def max_node_contribution_distribution(self):
+        max_node_contributions = self.attention_mat.argmax(dim=1)
+        
+        return Counter(max_node_contributions.tolist())
